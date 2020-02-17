791fcc0551b57ff5c78c8041f1ce47551f985cb7
Ricky-Castro <decastrohenrique@yahoo.com.br>
2020-02-17 20:27:43 -0300

    While

--- While - estrutura de repetição em Python.py
+++ While - estrutura de repetição em Python.py
@@ -9,7 +9,7 @@ Requisitos: Entender condições e operadores.
 while True: # loop infinito pois nunca será false
     nome = input('Qual é o seu nome?')
     print(f'Olá {nome}!')
-"""
+
 
 x = 0
 while x < 10:
@@ -21,5 +21,18 @@ while x < 10:
 
 
 print(f'Agora x é igual a {x}')
+"""
+
+contador = 1
+acomulador = 1
+
+while contador <= 10:
+    print(contador, acomulador)
+
+    acomulador = acomulador + contador
+    contador += 1
+else:
+    print('Cheguei no else')
+
 
 
