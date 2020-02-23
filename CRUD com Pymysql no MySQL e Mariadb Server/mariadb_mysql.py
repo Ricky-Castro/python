91572f3cc10bcc3d550618448927f09ad829afa8
Ricky-Castro <decastrohenrique@yahoo.com.br>
2020-02-23 15:12:42 -0300

    Praticando CRUD com pymsql

--- CRUD com Pymysql no MySQL e Mariadb Server/mariadb_mysql.py
+++ CRUD com Pymysql no MySQL e Mariadb Server/mariadb_mysql.py
@@ -1,21 +1,73 @@
 import pymysql.cursors
+from contextlib import contextmanager
 
-conexao = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    db='clientes',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
+
+@contextmanager
+def conecta():
+    conexao = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='@hdc*1111',
+        db='clientes',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        yield conexao
+    finally:
+        print('')
+        print('Conexão fechada!')
+        conexao.close()
+
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = ('INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES'
+#                '(%s, %s, %s, %s)')
+#         cursor.execute(sql, ('Jack', 'Smith', 112, 220))
+#         conexao.commit()
+
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = ('INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES'
+#                '(%s, %s, %s, %s)')
+#
+#         dados = [
+#             ('MURIEL', 'FIGUEIREDO', 19, 55),
+#             ('MAURO', 'ROBERTISON', 80, 55.5),
+#             ('ADRIANO', 'MANUEL', 83, 69),
+#         ]
+#
+#         cursor.executemany(sql, dados)
+#         conexao.commit()
+
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = "DELETE FROM clientes WHERE id = %s"
+#         cursor.execute(sql, (7,))
+#         conexao.commit()
+
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = "DELETE FROM clientes WHERE id IN (%s, %s, %s)"
+#         cursor.execute(sql, (5, 8, 10))
+#         conexao.commit()
+
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = "UPDATE clientes SET sobrenome=%s WHERE id=%s"
+#         cursor.execute(sql, ('SMITH', 6))
+#         conexao.commit()
 
 #cursor = conexao.cursor()
-with conexao.cursor() as cursor:
-    cursor.execute('SELECT * FROM clientes')
-    resultado = cursor.fetchall()
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM clientes ORDER By id ASC LIMIT 100')
+        resultado = cursor.fetchall()
 
-    for linha in resultado:
-        print(linha ['nome'], linha ['sobrenome'])
+        for linha in resultado:
+            print(linha)
+            #print(linha ['nome'], linha ['sobrenome'])
 
-conexao.close()
+#conexao.close()
 #cursor.close() CASO NAO USE O WITH
